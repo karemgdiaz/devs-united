2bed46eddf119f248ccf7c60a69c8d69d70055a3
Karem Gomez <karemgdiaz@gmail.com>
2021-12-04 22:42:22 -0300

    Primer commit alternate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh, hi.
+          Oh, hi. I'm the alternate. Call me maybe. 
         </p>
         <a
           className="App-link"
